069adae8b31c066a4104c6228f9c49b748a458b7
carlos-paz12 <capz.devel@gmail.com>
2025-01-07 08:51:07 -0300

    Rearrange dir structure

--- instruction-set-simulator/build/CMakeFiles/InstructionSetSimulator.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for InstructionSetSimulator.
-# This may be replaced when dependencies are built.
--- instruction-set-simulator/build/CMakeFiles/InstructionSetSimulator.dir/src/neander.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/InstructionSetSimulator.dir/src/neander.cpp.o: \
- /home/dovale/Second-Semester/Sem2/AC1/AC1/pcis/instruction-set-simulator/src/neander.cpp \
- /usr/include/stdc-predef.h \
- /home/dovale/Second-Semester/Sem2/AC1/AC1/pcis/instruction-set-simulator/src/../inc/neander.h
