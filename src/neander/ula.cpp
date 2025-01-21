dd56770dadd60ffe6b7d8f3b0ebd4dd6a5982cab
carlos-paz12 <capz.devel@gmail.com>
2025-01-21 14:50:04 -0300

    Delete unused files

--- src/neander/ula.cpp
+++ src/neander/ula.cpp
@@ -13,4 +13,15 @@ byte ULA::not_operation(const byte x) { return (~x); }
 
 byte ULA::y_operation(const byte x, const byte y) { return y; }
 
+/*
+    ///!!! IMPLEMENTAR ISSO EM ALGUM LUGAR
+
+    if (AC & 0x80) ///[!] Se o bit mais significativo de AC for 1, o número é negativo (complemento de dois).
+        ///[!] Flag N ativada se o AC for negativo.
+        flag = Flag::FLAG_N;
+    else if (AC == 0)
+        ///[!] Flag Z ativada se o AC for zero.
+        flag = Flag::FLAG_Z;
+*/
+
 #endif
