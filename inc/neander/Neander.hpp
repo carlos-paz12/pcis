7324827ea22d42a77f5b7cbe1adb87f867064c3e
carlos-paz12 <capz.devel@gmail.com>
2025-01-21 21:38:56 -0300

    Add alias to byte

--- inc/neander/Neander.hpp
+++ inc/neander/Neander.hpp
@@ -9,6 +9,8 @@
 #include "Memoria.hpp"
 #include "ULA.hpp"
 
+using byte = std::uint8_t;
+
 class PC;
 
 class PO /// Parte Operativa
@@ -17,12 +19,20 @@ public:
   Memoria &memoria;
   PC &pc;
 
-  uint8_t AC;   ///!> Registrador acumulador.
-  uint8_t REM;  ///!> Registrador que armazena o endereço de uma posição de memória (registrador de endereço de memória)
-  uint8_t RDM;  ///!> Registrador que armazena o dado que será lido ou escrito na memória (registrador de dados de memória).
+  byte AC;   ///!> Registrador acumulador.
+  byte REM;  ///!> Registrador que armazena o endereço de uma posição de memória (registrador de endereço de memória)
+  byte RDM;  ///!> Registrador que armazena o dado que será lido ou escrito na memória (registrador de dados de memória).
   Instrucao RI; ///!> Registrador que armazena instrução a ser executada (registrador de instruções).
 
   ULA ula; ///!> Unidade Lógica e Aritmética.
+
+  PO(Memoria &memoria, PC &pc);
+
+  void fetch();
+
+  void decode();
+
+  void execute();
 
   void STA();
 
@@ -43,16 +53,6 @@ public:
   void JZ();
 
   void reset();
-
-  PO(Memoria &memoria, PC &pc);
-
-  void fetch();
-
-  void decode();
-
-  void execute();
-
-  void printstate();
 };
 
 class PC /// Parte de Controle
@@ -61,34 +61,34 @@ public:
   Memoria &memoria;
   PO &po;
 
-  uint8_t pc; ///!> Registrador que armazena o endereço da próxima instrução a ser executada (contador de programa).
-  uint8_t cc; ///!> Registrador que armazena a quantidade de ciclos do processador (contador de ciclos).
+  byte pc; ///!> Registrador que armazena o endereço da próxima instrução a ser executada (contador de programa).
+  byte cc; ///!> Registrador que armazena a quantidade de ciclos do processador (contador de ciclos).
 
   Flag flag; ///!> Flag de controle.
 
   PC(Memoria &memoria, PO &po);
 
-  uint8_t get_pc()
+  byte get_pc()
   {
     return pc;
   }
 
-  uint8_t incrementa_pc()
+  byte incrementa_pc()
   {
     return ++pc;
   }
 
-  uint8_t carga_pc(uint8_t val)
+  byte carga_pc(uint8_t val)
   {
     pc = val;
   }
 
-  uint8_t get_cc()
+  byte get_cc()
   {
     return ++cc;
   }
 
-  uint8_t incrementa_cc()
+  byte incrementa_cc()
   {
     return ++cc;
   }
@@ -99,15 +99,12 @@ class Neander
 private:
   Memoria &memoria;
   PO po;
-  std::vector<int> registradores; // Adicionando os registradores
   PC pc;
 
 public:
   Neander(Memoria &memoria);
 
   void fetch_decode_execute();
-
-  void print();
 };
 
 #endif /// NEANDER_H
